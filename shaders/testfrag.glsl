40c786f40abdc7a5f1d4c4339ee229a0116623f3
Reno50 <59569424+Reno50@users.noreply.github.com>
2023-01-30 17:52:53 -0700

    Somew more shader stuff

--- shaders/testfrag.glsl
+++ shaders/testfrag.glsl
@@ -3,6 +3,35 @@ out vec4 FragColor;
   
 in vec4 vertexColor;
 
+float triangleDist(vec2 p, float width, float height)
+{
+	vec2 n = normalize(vec2(height, width / 2.0));
+	return max(	abs(p).x*n.x + p.y*n.y - (height*n.y), -p.y);
+}
+
+float triangleDist(vec2 p, float radius)
+{
+	return max(	abs(p).x * 0.866025 + 
+			   	p.y * 0.5, -p.y) 
+				-radius * 0.5;
+}
+
+float boxDist(vec2 p, vec2 size, float radius)
+{
+	size -= vec2(radius);
+	vec2 d = abs(p) - size;
+  	return min(max(d.x, d.y), 0.0) + length(max(d, 0.0)) - radius;
+}
+
+float lineDist(vec2 p, vec2 start, vec2 end, float width)
+{
+	vec2 dir = start - end;
+	float lngth = length(dir);
+	dir /= lngth;
+	vec2 proj = max(0.0, min(lngth, dot((start - p), dir))) * dir;
+	return length( (start - p) - proj ) - (width / 2.0);
+}
+
 void main()
 {
     FragColor = vertexColor;
